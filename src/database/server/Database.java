72ba4ccb6a36d2384d323c761e378dbec74ee751
程嘉梁 <chengjl16@163.com>
2019-04-27 20:03:04 +0800

    进行注释的补全与小bug的修复

--- src/database/server/Database.java
+++ src/database/server/Database.java
@@ -13,29 +13,32 @@ import database.persist.DBTableManager;
  */
 public class Database
 {
-	public String dbname;
+	public String dbname;//数据库名称
     private DBTableManager table_manager;//数据表管理器
     private DBPageBuffer page_buffer;//缓冲区管理器
 
     /**
      * 构造函数
-     * 
+     * @param m 数据库管理类
      */
     public Database(DatabaseManager m)
     {
     	table_manager = new DBTableManager(m);
     	page_buffer = new DBPageBuffer(m, DBPageBuffer.DEFAULT_PAGES);
     }
-    public void clearAll() {
+    public void clearAll()
+    {
     	close();
     	table_manager.clearAll();
     	page_buffer.clearAll();
     	page_buffer.createBuffer(DBPageBuffer.DEFAULT_PAGES);
     }
+    
     /**
-     * 关闭数据库，持久化表的元信息和数据，退出进程需要显示调用
+     * 关闭数据库，持久化表的元信息和数据，退出进程需要显式调用
      */
-    public void close() {
+    public void close()
+    {
     	table_manager.writeSchema();
     	page_buffer.writeOperatedPages();
     }
--- src/database/server/DatabaseManager.java
+++ src/database/server/DatabaseManager.java
@@ -2,10 +2,16 @@ package database.server;
 
 import java.io.*;
 
-public class DatabaseManager {
+public class DatabaseManager
+{
 	public String prefix; //文件地址的前缀
 	public Database database;
-	public DatabaseManager() {
+	
+	/**
+	 * 构造函数
+	 */
+	public DatabaseManager()
+	{
 		database = new Database(this);
 		setDBname("default");
 		createSchema(database.dbname);
@@ -15,38 +21,40 @@ public class DatabaseManager {
 	 * 设置数据库名
 	 * @param name
 	 */
-	private void setDBname(String name) {
+	private void setDBname(String name)
+	{
 		database.dbname = name;
 		prefix = getPrefix(name);
 	}
 	/**
 	 * 根据数据库名获取地址前缀
 	 * @param name
-	 * @return
+	 * @return 地址前缀字符串
 	 */
-	private String getPrefix(String name) {
-		return "./db/" + name + '/';
-	}
+	private String getPrefix(String name) { return "./db/" + name + '/'; }
+	
 	/**
 	 * 检查指定数据库目录下的schema.txt是否存在
 	 */
-	private boolean schemaExists(String name) {
-		return new File(getPrefix(name) + "/schema.txt").exists();
-	}
+	private boolean schemaExists(String name) { return new File(getPrefix(name) + "/schema.txt").exists(); }
+	
 	/**
 	 * 创建指定数据库目录和其下的schema.txt
-	 * 返回true说明成功创建 返回false说明本来就已经存在该目录下的schema.txt
 	 * @param name
-	 * @return
+	 * @return true说明成功创建/false说明本来就已经存在该目录下的schema.txt
 	 */
-	private boolean createSchema(String name) {
-		if (schemaExists(name)) {
+	private boolean createSchema(String name)
+	{
+		if (schemaExists(name))
+		{
 			return false;
 		}
 		String prefix = getPrefix(name);
 		File dbpath = new File(prefix);
-		if (!dbpath.exists()) {
-			try {
+		if (!dbpath.exists())
+		{
+			try
+			{
 				dbpath.mkdirs();
 			}
 			catch(Exception e)
@@ -55,8 +63,10 @@ public class DatabaseManager {
             }
 		}
 		File schema_file = new File(prefix + "/schema.txt");
-		if (!schema_file.exists()) {
-			try {
+		if (!schema_file.exists())
+		{
+			try
+			{
 				schema_file.createNewFile();
 			}
 			catch(Exception e)
@@ -68,66 +78,86 @@ public class DatabaseManager {
 	}
 	/**
 	 * 切换数据库
-	 * @param name
+	 * @param name 数据库名称
 	 */
-	public void switchDatabase(String name) throws Exception {
-		if (!schemaExists(name)) {
+	public void switchDatabase(String name) throws Exception
+	{
+		if (!schemaExists(name))
+		{
 			throw new Exception("Invalid Database: " + name);
 		}
 		database.clearAll();
 		setDBname(name);
 		database.getTableManager().loadSchema();
 	}
+	
 	/**
 	 * 增加数据库
-	 * @param name
+	 * @param name 数据库名称
 	 */
-	public void addDatabase(String name) throws Exception {
-		if (schemaExists(name)) {
+	public void addDatabase(String name) throws Exception
+	{
+		if (schemaExists(name))
+		{
 			throw new Exception("Database already exists: " + name);
 		}
 		createSchema(name);
 	}
+	
 	/**
 	 * 删除数据库，不能删除当前数据库
 	 * @param name
 	 * @throws Exception
 	 */
-	public void removeDatabase(String name) throws Exception {
-		if (name == database.dbname) {
+	public void removeDatabase(String name) throws Exception
+	{
+		if (name.equals(database.dbname))
+		{
 			throw new Exception("Can't delete current DB.");
 		}
-		if (!schemaExists(name)) {
+		if (!schemaExists(name))
+		{
 			throw new Exception("Invalid Database: " + name);
 		}
-		if (!deleteFolder(getPrefix(name))) {
+		if (!deleteFolder(getPrefix(name)))
+		{
 			throw new Exception("Delete failure!");
 		};
 	}
+	
 	/**
-	 * 删除文件夹 返回是否成功
-	 * @param sPath
-	 * @return
+	 * 删除文件夹
+	 * @param sPath 文件夹路径
+	 * @return 是否成功删除
 	 */
-	private boolean deleteFolder(String sPath) {  
+	private boolean deleteFolder(String sPath)
+	{  
 	    boolean flag = false;  
 		File folder = new File(sPath);  
-		if (!folder.exists()) {  // 不存在返回 false  
+		if (!folder.exists())
+		{ 
 			return flag;  
-		} else {  
+		}
+		else
+		{  
 			File[] files = folder.listFiles();  
-			for (int i = 0; i < files.length; i++) {
-				if (files[i].isFile()) {  
+			for (int i = 0; i < files.length; i++)
+			{
+				if (files[i].isFile())
+				{  
 					flag = files[i].delete(); 
 					if (!flag) break;
-				} else {
+				}
+				else
+				{
 					flag = deleteFolder(files[i].getAbsolutePath());
 					if (!flag) break;
 				}
 			}
 		}
 		if (!flag) return false;
-		if (folder.delete()) {
+		if (folder.delete())
+		{
 			return true;
 		}
 		return false;
